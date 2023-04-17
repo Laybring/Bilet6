4c9752177100bf8ff4ac6e2ca8065119631df6f7
Laybring <dimarulyov@gmail.com>
2023-04-17 05:35:53 +0300

    Добавьте файлы проекта.

---
+++ Ekzam_6/Connecting.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekzam_6
+{
+    public class Connecting
+    {
+
+        private static string Connections = @"Data Source=LAYBRING\MSSQLSERVER02; Initial Catalog=ExamUsers;Integrated Security=True";
+        private SqlConnection connection = new SqlConnection(Connections);
+
+        public void Open() 
+        {
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+
+                connection.Open();
+
+            }
+        
+        }
+
+        public void Close() 
+        {
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+
+                connection.Close();
+
+            }
+        
+        }
+
+        public SqlConnection GetConnection() 
+        {
+        
+        return connection;
+        
+        }
+
+    }
+}
---
+++ Ekzam_6/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ekzam_6
+{
+    public partial class Form1 : Form
+    {
+
+        Connecting connect = new Connecting();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+
+            dataGridView1.Columns.Add("S1","Логин");
+            dataGridView1.Columns.Add("S2", "Пароль");
+
+            SqlCommand command = new SqlCommand("select * from USERS", connect.GetConnection());
+
+            using (var reader = command.ExecuteReader()) 
+            {
+
+                while (reader.Read())
+                {
+
+                    dataGridView1.Rows.Add(reader["PASSWORD"].ToString(), reader["LOGIN"].ToString());
+
+                }
+            
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "*.txt";
+            saveFileDialog.Filter = "txt|*.txt";
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK ) 
+            {
+                FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                StreamWriter streamWriter = new StreamWriter(fs);
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    for (int i = 0; i < dataGridView1.Rows[j].Cells.Count; i++)
+                    {
+                        streamWriter.Write(dataGridView1.Rows[j].Cells[i].Value + "     ");
+                    }
+
+                    streamWriter.WriteLine();
+
+                }
+
+                streamWriter.Close();
+                fs.Close();
+
+                MessageBox.Show("Файл успешно сохранен");
+
+                connect.Close();
+            }
+        }
+    }
+}
